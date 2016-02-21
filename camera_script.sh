0937ca13c5ee8886e0c8637c1196552989824998
Roman <ryinedgon@mail.ru>
2016-02-21 17:45:18 +0200

    Move image was move to separate function

--- camera_script.sh
+++ camera_script.sh
@@ -8,22 +8,31 @@ else
 fi
 
 if [ ! -z "$1" ]; then
-  echo "Parameter 1:" $1
-  dfilt=$1
+	echo "Parameter 1 (Year_Month):" $1
+	dfilt=$1
 else
-  echo "No parameter 1"
-  dfilt="14_6_7"
-  exit 1
+	echo "No parameter 1 (Year_Month)"
+	dfilt="14_6_7"
+	exit 1
 fi
 
 list_day="1"
 if [ ! -z "$2" ]; then
-  echo "Parameter 2:" $2
+  echo "Parameter 2 (\"List Days\"):" $2
   list_day=$2
 else
-  echo "No parameter 2"
+  echo "No parameter 2(\"List Days\")"
   exit 1
 fi
+
+function move_image {
+	echo $filename;
+	shortname=$(basename $filename);
+	if [ $filename != "./$dfilt/$shortname" ]; then
+	echo "mv $filename";
+	mv $filename $dfilt;
+	fi;
+}
 
 #IFS=' ' read -a array <<< "$list_day"
 #array=(${list_day// / })
@@ -34,22 +43,15 @@ dfilt2=$dfilt
 
 for c in $list_day
 do
-dfilt=$dfilt2"_"$c
-echo $dfilt
-#break
-if [ ! -d $dfilt ]; then
-   mkdir $dfilt
-else
-  echo "directory exist"
-fi
+	dfilt=$dfilt2"_"$c
+	echo $dfilt
+	#break
+	if [ ! -d $dfilt ]; then
+	   mkdir $dfilt
+	else
+	  echo "directory exist"
+	fi
 
-find . -iname "IPC_IPCamera_${dfilt}_*" | while read filename; do \
- echo $filename; \
- shortname=$(basename $filename); \
- if [ $filename != "./$dfilt/$shortname" ]; then \
-   echo mv $filename; \
-   mv $filename $dfilt; \
- fi; \
-done;
+find . -iname "IPC_IPCamera_${dfilt}_*" | while read filename; do move_image $filename; done;
 
 done;
